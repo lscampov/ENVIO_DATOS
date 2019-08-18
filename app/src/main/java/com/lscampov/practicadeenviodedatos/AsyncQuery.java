164b46afb7c6abb203f44d03335e84438d69f7ef
Lorena Campoverde <lscampov@espol.edu.ec>
2019-08-17 19:16:32 -0500

    Commit inicial

---
+++ app/src/main/java/com/lscampov/practicadeenviodedatos/AsyncQuery.java
@@ -0,0 +1,94 @@
+package com.lscampov.practicadeenviodedatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class AsyncQuery extends AsyncTask<String[],Void,String[]> {
+
+    private Connection conexionMySQL=null;
+    private Statement st = null;
+    //private ResultSet rs = null;
+
+    @Override
+    protected String[] doInBackground(String[]... datos) {
+        String descripcion = datos[0][5];
+        String precio = datos[0][6];
+        String cantidad = datos[0][7];
+        String id_factura = datos[0][8];
+        //String resultadoSQL = "";
+        //String[] totalResultadoSQL = null;
+        //int numColumnas = 0;
+        //int numFilas = 0;
+        String SERVIDOR = datos[0][0];
+        String PUERTO = datos[0][1];
+        String BD = datos[0][2];
+        String USUARIO = datos[0][3];
+        String PASSWORD = datos[0][4];
+
+        try{
+            conexionMySQL = DriverManager.getConnection("jdbc:mysql://" + SERVIDOR + ":" + PUERTO + "/" + BD,
+                    USUARIO,PASSWORD);
+
+            st = conexionMySQL.createStatement();
+            st.executeUpdate("INSERT INTO Lote (descripcion, precio_unitario, cantidad, id_factura) VALUES " +
+                    "('" + descripcion + "','" + precio + "','" + cantidad + "','" + id_factura + "')");
+            /*rs.last();
+            numFilas = rs.getRow();
+            if(numFilas == 0)
+            {
+                resultadoSQL = "No se ha producido ningún resultado. Revise la consulta realizada.\n";
+            }else
+            {
+                for(int i=1;i<=numColumnas;i++){
+                    if(i>1) resultadoSQL += ",";
+                    resultadoSQL += rs.getMetaData().getColumnName(i);
+                }
+                resultadoSQL += "\n";
+                rs.beforeFirst();
+                while (rs.next())
+                {
+                    numColumnas = rs.getMetaData().getColumnCount();
+                    for(int i=1;i<=numColumnas;i++){
+                        if(i>1) resultadoSQL += ",";
+                        resultadoSQL += rs.getString(i);
+                    }
+                    resultadoSQL += "\n";
+                }
+            }
+            totalResultadoSQL = new String[]{ resultadoSQL,String.valueOf(numFilas),String.valueOf(numColumnas)};*/
+
+        }catch(SQLException ex)
+        {
+            Log.d("Error de conexión", ex.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                // if(rs != null)
+                //  {
+                //      rs.close();
+                //  }
+                st.close();
+                conexionMySQL.close();
+            } catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+}
+
+
---
+++ app/src/main/java/com/lscampov/practicadeenviodedatos/MainActivity.java
@@ -0,0 +1,68 @@
+package com.lscampov.practicadeenviodedatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String serverIP = "remotemysql.com";
+    private String port = "3306";
+    private String userMySQL = "X7V9l1RypJ";
+    private String pwdMySQL = "osJBbaNPlO";
+    private String database = "X7V9l1RypJ";
+    private String[] datosConexion = null;
+    private EditText consulta1;
+    private EditText consulta2;
+    private EditText consulta3;
+    private EditText consulta4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        consulta1= (EditText)findViewById(R.id.editText);
+        consulta2= (EditText)findViewById(R.id.editText2);
+        consulta3= (EditText)findViewById(R.id.editText3);
+        consulta4= (EditText)findViewById(R.id.editText4);
+    }
+
+    public void mostrarResultados(View view)
+    {
+        //String[] resultadoSQL = null;
+        try{
+            String driver = "com.mysql.jdbc.Driver";
+            Class.forName(driver).newInstance();
+
+            datosConexion = new String[]{
+                    serverIP,
+                    port,
+                    database,
+                    userMySQL,
+                    pwdMySQL,
+                    consulta1.getText().toString(),
+                    consulta2.getText().toString(),
+                    consulta3.getText().toString(),
+                    consulta4.getText().toString()
+            };
+
+
+            new AsyncQuery().execute(datosConexion);
+            Toast.makeText(MainActivity.this,"Conexión Establecida", Toast.LENGTH_LONG).show();
+
+            //String resultadoConsulta = resultadoSQL[0];
+            //String numFilas = resultadoSQL[1];
+            //String numColumnas = resultadoSQL[2];
+            //consulta.setText(resultadoConsulta + "\nNúmero de filas devueltas: " + numFilas + "\nNúmero de columnas devueltas: " + numColumnas);
+
+
+        }catch(Exception ex)
+        {
+            Toast.makeText(this, "Error: "
+                    + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
